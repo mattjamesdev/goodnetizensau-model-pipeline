711f5c61be19f975c73ddffe9437dfaa4f00a781
mattjamesdev <57205718+mattjamesdev@users.noreply.github.com>
2022-10-08 22:43:54 +1100

    Updated to use a simpler neural network.

--- app/rnn.py
+++ app/rnn.py
@@ -3,9 +3,8 @@ from pathlib import Path
 
 import tensorflow as tf
 
-
 current_dir = Path(__file__).resolve(strict=True).parent
-RNN_MODELS_DIR = current_dir / Path("models/rnn")
+NN_MODELS_DIR = current_dir / Path("models/nn")
 
 
 # Set tensorflow log level
@@ -16,26 +15,24 @@ tf.get_logger().setLevel("ERROR")
 def get_encoder(vocab_size):
     """
     Create the encoder and load its weights.
-
     Parameters
     ----------
     vocab_size : int
         Vocabulary size.
     """
-    # Define the TF/IDF encoder
+    # Define the Tokenizer encoder
     encoder = tf.keras.layers.TextVectorization(
         max_tokens=vocab_size, output_sequence_length=280
     )
     # Unpickle the weights and set them
-    # with open("models/rnn/Encoder_Weights", "rb") as fp:
-    with open(RNN_MODELS_DIR / Path("Encoder_Weights"), "rb") as fp:
+    with open(NN_MODELS_DIR / Path("Encoder_Weights"), "rb") as fp:
         w = pickle.load(fp)
     encoder.set_weights(w)
     return encoder
 
 
 # Create the model
-def build_rnn_model(encoder):
+def build_nn_model(encoder):
     """
     Build the model and load its weights.
 
@@ -55,23 +52,22 @@ def build_rnn_model(encoder):
             tf.keras.layers.Embedding(
                 input_dim=len(encoder.get_vocabulary()) + 1,
                 output_dim=100,
+                # weights = [emb_matrix],
+                # Use masking to handle the variable sequence lengths
                 mask_zero=False,
             ),
-            tf.keras.layers.Bidirectional(
-                tf.keras.layers.LSTM(64, return_sequences=True, dropout=0.01)
-            ),
-            tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, dropout=0.01)),
+            tf.keras.layers.Lambda(lambda x: tf.math.reduce_sum(x, axis=1)),
             tf.keras.layers.Dropout(0.2),
-            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dense(100, activation="relu"),
             tf.keras.layers.Dropout(0.2),
-            tf.keras.layers.Dense(64, activation="relu"),
+            tf.keras.layers.Dense(50, activation="relu"),
             tf.keras.layers.Dropout(0.2),
             tf.keras.layers.Dense(3, activation="softmax"),
         ]
     )
 
     # Get the model weights
-    model_path = RNN_MODELS_DIR / Path("weights")
+    model_path = NN_MODELS_DIR / Path("weights")
     model.load_weights(str(model_path))
 
     # Compile the model
@@ -79,6 +75,7 @@ def build_rnn_model(encoder):
         loss=tf.keras.losses.CategoricalCrossentropy(
             from_logits=False, label_smoothing=0.01
         ),
+        # loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False), #BinaryCrossentropy(from_logits=True),
         optimizer=tf.keras.optimizers.Adam(1e-4),
         metrics=["accuracy"],
     )
